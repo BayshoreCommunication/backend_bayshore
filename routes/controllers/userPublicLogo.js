05579b2bd8a565cf6718d330288b74d661d30a30
Faisal Ahamed Rahad <155021870+rahad-dev24@users.noreply.github.com>
2024-12-29 14:28:37 +0600

    update user public logo route

--- routes/controllers/userPublicLogo.js
+++ routes/controllers/userPublicLogo.js
@@ -1,24 +1,19 @@
 import Logo from "../../database/models/Logo.js";
+import User from "../../database/models/User.js";
 
-
-const user_public_logo= async(req,res)=>{
-
-
-    try{
-        const logoFound = await Logo.findOne({name:"logo"})
-        if(logoFound){
-            return res.status(200).json({ success: logoFound })
-
-        }
-        return res.status(400).json({ error: "something wrong" });
-
-
+const user_public_logo = async (req, res) => {
+  try {
+    const logoFound = await Logo.findOne({ name: "logo" });
+    const user = await User.find();
+    if (logoFound) {
+      return res
+        .status(200)
+        .json({ data: { success: logoFound, color: user[0].color } });
     }
-    catch(e){
     return res.status(400).json({ error: "something wrong" });
-
-    }
-
-}
+  } catch (e) {
+    return res.status(400).json({ error: "something wrong" });
+  }
+};
 
 export default user_public_logo;
